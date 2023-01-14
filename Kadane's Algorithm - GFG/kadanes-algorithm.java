5d7953a5e4ea578f7f4143dd80b24ef46ef44e20
shitaanshus27 <122525450+shitaanshus27@users.noreply.github.com>
2023-01-14 22:41:57 +0530

    Added solution - LeetHub

---
+++ Kadane's Algorithm - GFG/kadanes-algorithm.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.io.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxSubarraySum() function
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+       int max_curr=0,max_so=Integer.MIN_VALUE;
+       for(int i=0;i<n;i++)
+       {
+           max_curr+=arr[i];
+           if(max_curr>max_so)
+                max_so=max_curr;
+            
+           if(max_curr<0)
+                max_curr=0;
+       }
+     
+    return max_so;    
+    }
+    
+}
+
