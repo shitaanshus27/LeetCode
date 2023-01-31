34b2c12e759853714455e5cb00b9655aba56ddf1
shitaanshus27 <122525450+shitaanshus27@users.noreply.github.com>
2023-01-31 13:26:39 +0530

    Added solution - LeetHub

---
+++ Print Anagrams Together - GFG/print-anagrams-together.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	    int t=Integer.parseInt(br.readLine().trim());
+	    while(t > 0)
+	    {
+	        int n= Integer.parseInt(br.readLine().trim());
+	        String x = br.readLine().trim();
+	        String string_list[] = x.split(" ",n);
+	        
+	        Solution ob = new  Solution();
+	        
+	        List <List<String>> ans = ob.Anagrams(string_list);
+	        
+	        Collections.sort(ans, new Comparator<List<String>>(){
+            public int compare(List<String> l1, List<String> l2) {
+                    String s1 =  l1.get(0);
+                    String s2 = l2.get(0);
+                    
+                    return s1.compareTo(s2);
+                }
+            });
+	        
+	        for(int i=0;i<ans.size();i++)
+	        {
+	            for(int j=0;j<ans.get(i).size();j++)
+	            {
+	                System.out.print(ans.get(i).get(j) + " ");
+	            }
+	            System.out.println();
+	        }
+	       
+	       
+            t--;
+	    }
+	}
+    
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public List<List<String>> Anagrams(String[] string_list) {
+        if(string_list==null && string_list.length==0) return new ArrayList<>();
+        Map<String,List<String>> mp=new HashMap<>();
+        for(String str:string_list)
+        {
+             char[] ch=str.toCharArray();
+             Arrays.sort(ch);
+             String key=String.valueOf(ch);
+             if(!mp.containsKey(key))
+                mp.put(key,new ArrayList<String>());
+            mp.get(key).add(str);    
+        }
+    return new ArrayList<>(mp.values());    
+    }
+}
