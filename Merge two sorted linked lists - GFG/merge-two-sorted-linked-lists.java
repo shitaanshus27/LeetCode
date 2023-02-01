054b7ef5fa3c10fc5696c6f154cbbe8004176781
shitaanshus27 <122525450+shitaanshus27@users.noreply.github.com>
2023-02-01 11:40:17 +0530

    Added solution - LeetHub

---
+++ Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.java
@@ -0,0 +1,126 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+
+
+class MergeLists
+{
+    Node head;
+
+
+
+  /* Function to print linked list */
+   public static void printList(Node head)
+    {
+        
+        while (head!= null)
+        {
+           System.out.print(head.data+" ");
+           head = head.next;
+        }  
+        System.out.println();
+    }
+	
+	 
+ 
+     /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+       
+         
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t>0)
+         {
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			Node head1 = new Node(sc.nextInt());
+            Node tail1 = head1;
+            for(int i=0; i<n1-1; i++)
+            {
+                tail1.next = new Node(sc.nextInt());
+                tail1 = tail1.next;
+            }
+			Node head2 = new Node(sc.nextInt());
+            Node tail2 = head2;
+            for(int i=0; i<n2-1; i++)
+            {
+                tail2.next = new Node(sc.nextInt());
+                tail2 = tail2.next;
+            }
+			
+			LinkedList obj = new LinkedList();
+			Node head = obj.sortedMerge(head1,head2);
+			printList(head);
+			
+			t--;
+			
+         }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+  Merge two linked lists 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+class LinkedList
+{
+    //Function to merge two sorted linked list.
+    Node sortedMerge(Node head1, Node head2) {
+     List<Integer> list1=new ArrayList<>();
+     //List<Integer> list=new ArrayList<>();
+     Node new_head=null;
+     Node curr=null;
+     Node temp=head1;
+     while(temp!=null)
+     {
+         list1.add(temp.data);
+         temp=temp.next;
+     }
+     Node temp1=head2;
+     while(temp1!=null)
+     {
+         list1.add(temp1.data);
+         temp1=temp1.next;
+     }
+     Collections.sort(list1);
+     for(int i=0;i<list1.size();i++)
+     {
+         Node new_node=new Node(list1.get(i));
+         if(new_head==null)
+         {
+             new_head=new_node;
+             curr=new_node;
+         }
+         else
+         {
+             curr.next=new_node;
+             curr=new_node;
+         }
+     }
+     return new_head;
+   } 
+}
