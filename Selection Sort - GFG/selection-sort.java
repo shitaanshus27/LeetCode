7e9730ae0d4da94043f9ea34f062a1323e095ee4
shitaanshus27 <122525450+shitaanshus27@users.noreply.github.com>
2023-02-11 15:08:29 +0530

    Added solution - LeetHub

---
+++ Selection Sort - GFG/selection-sort.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n]; 
+			for(int i=0;i<n;i++)
+			{
+				arr[i] = sc.nextInt();
+			}
+		
+			Solution obj = new Solution();
+			obj.selectionSort(arr, n);
+			
+			for(int i=0;i<n;i++)
+		    	System.out.print(arr[i]+" ");
+		    System.out.println();
+			t--;
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+	int  select(int arr[], int i)
+	{
+       int min=i;
+       for(int j=1+i;j<arr.length;j++)
+       {
+           if(arr[j]<arr[min])
+             { 
+               min=j;
+             }
+       }
+     return min;
+	}
+	
+	void selectionSort(int arr[], int n)
+	{
+	     for(int i=0;i<n;i++)
+     {
+         int k=select(arr,i);
+         int temp=arr[i];
+         arr[i]=arr[k];
+         arr[k]=temp;
+     }
+    }
+}
