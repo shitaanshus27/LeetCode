30383028d579416c7ab27899a56114ca2e52a3a2
shitaanshus27 <122525450+shitaanshus27@users.noreply.github.com>
2023-02-17 17:08:51 +0530

    Added solution - LeetHub

---
+++ Combination Sum - GFG/combination-sum.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    static ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+    public static void main (String[] args)throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine());
+        
+        while(t-- >0)
+        {
+            String str[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            ArrayList<Integer> list = new ArrayList<>();
+            str = br.readLine().trim().split(" ");
+            for(int i = 0; i <n ;i++)
+                list.add(Integer.parseInt(str[i]));
+            str = br.readLine().trim().split(" ");    
+            int sum = Integer.parseInt(str[0]);
+            
+            Solution ob = new Solution();
+            
+            res = ob.combinationSum(list, sum);
+            if (res.size() == 0) {
+    			out.print("Empty");
+    		}
+ 
+    		// Print all combinations stored in res.
+    		for (int i = 0; i < res.size(); i++) {
+    			if (res.size() > 0) {
+    				out.print("(");
+    				List<Integer> ij = res.get(i);
+    				for (int j = 0; j < ij.size(); j++) {
+    				    
+    					out.print(ij.get(j));
+    					if(j < ij.size()-1)
+    					 out.print(" ");
+    				}
+    				out.print(")");
+    			}
+    		}
+    		out.println();
+    		res.clear();
+	    }
+	    out.flush();
+    }
+    
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to return a list of indexes denoting the required 
+    //combinations whose sum is equal to given number.
+    static ArrayList<ArrayList<Integer>> combinationSum(ArrayList<Integer> A, int B)
+    {
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+        Collections.sort(A);
+        combine(ans,A,B,0,new ArrayList<>());
+        return ans;
+    }
+    static void combine(ArrayList<ArrayList<Integer>> ans,ArrayList<Integer> A,int B,int idx,List<Integer> ds)
+    {
+        if(B==0)
+            {
+            if(!ans.contains(ds))
+                 ans.add(new ArrayList<>(ds));
+                return;
+            }
+        for(int i=idx;i<A.size();i++)
+        {
+            if(i>idx && A.get(i)==A.get(i-1)) continue;
+            if(A.get(i)>B) break;
+            ds.add(A.get(i));
+            combine(ans,A,B-A.get(i),i,ds);
+            ds.remove(ds.size()-1);
+        }
+         //combine(ans,A,B,idx+1,ds);
+    }
+}
