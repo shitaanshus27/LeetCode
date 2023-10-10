d971b2e8d0b1ba7fcde0487ff62609cd369f01f3
Shitaanshu Singh <122525450+shitaanshus27@users.noreply.github.com>
2023-10-10 12:26:32 +0530

    Added solution - LeetHub

---
+++ Insert in a Sorted List - GFG/insert-in-a-sorted-list.java
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+            int n = sc.nextInt();
+            
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution g = new Solution();
+            head = g.sortedInsert(head,k);
+            
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+class Solution {
+    Node sortedInsert(Node head1, int key) {
+       if(head1==null)
+       {
+           return new Node(key);
+       }
+       
+      if(head1.data>key)
+      {
+          Node temp=new Node(key);
+          temp.next=head1;
+          head1=temp;
+          
+         return head1; 
+      }
+    
+       Node temp=head1;
+       Node prev=null;
+       while(temp!=null)
+       {
+          if(temp.data>key)
+          {
+            Node tem=new Node(key);
+            tem.next=temp;
+            prev.next=tem;
+            break;
+          }
+         prev=temp;
+         temp=temp.next; 
+       }
+       if(temp==null)
+       {
+         Node newNode=new Node(key);
+         prev.next=newNode;
+       }
+     return head1;  
+    }
+}
